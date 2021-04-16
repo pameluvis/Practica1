5dc5db4a8a5fd5a0ca18f8fa827e50a5695e0a52
pamela <80125129+pameluvis@users.noreply.github.com>
2021-04-15 23:51:38 -0500

    Add files via upload
    Primera subida de código

---
+++ Practica1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0, fact = 0;
+      
+            Console.Write("Ingresa el número del que deseas obtener su factorial: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero == 0)
+            {
+                Console.WriteLine("El factorial de 0 es: 1");
+            }
+            else
+            {
+                fact = numero;
+                for (int i=numero; i>1; i--)
+                {  
+                    fact = fact * (i - 1);
+                }
+                Console.WriteLine("El factorial de " +numero+" es: "+fact);
+            }
+            Console.ReadKey();
+
+           
+        }
+    }
+}
